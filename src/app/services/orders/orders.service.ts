6ce72a243dc30a145cab1c4859ab806ad6ea8d9c
ZachRauch <zach.rauch@outlook.com>
2022-12-29 15:58:17 -0600

    Proof of concept for orders tables and sudo code for http requests

---
+++ src/app/services/orders/orders.service.ts
@@ -0,0 +1,71 @@
+import { Injectable } from '@angular/core';
+import { Order } from 'src/app/data/orders';
+import { HttpClient } from '@angular/common/http';
+import { take } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AccountService } from '../account.service';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrdersService {
+
+  constructor(private http: HttpClient,
+    private _snackBar: MatSnackBar,
+    private accountService: AccountService) {
+      const userRole = localStorage.getItem('role')
+     }
+
+  public userId: number | undefined;
+  private orders: Order[] = []
+  private userOrders: Order[] = []
+  public userRole: string | undefined
+  
+  private showError(message: string): void {
+    this._snackBar.open(message, undefined, {duration: 10000})
+  }
+
+private loadOrders(): void {
+    if (this.userRole = "shopkeeper") {
+      this.loadAllOrders()
+    } else {
+      this.loadUserOrders()
+    }
+  }
+
+  private loadAllOrders(): void {
+    this.http.get<Order[]>(`http://localhost:8080/orders`)
+      .pipe(take(1))
+      .subscribe({
+        next: orders => {
+          this.orders = orders
+        },
+        error: () => {
+          this.showError('Oops, something went wrong')
+        }
+      })
+  }
+
+  private loadUserOrders(): void {
+    this.http.get<Order[]>(`http://localhost:8080/orders?userId=${this.userId}`)
+      .pipe(take(1))
+      .subscribe({
+        next: orders => {
+          this.userOrders = orders
+          },
+        error: () => {
+          this.showError('Oops, something went wrong')
+        }
+      })
+  }
+
+  public getOrders(): Order[] {
+    return this.orders
+  }
+
+  getUserOrders() {
+    return this.userOrders
+  }
+
+}
