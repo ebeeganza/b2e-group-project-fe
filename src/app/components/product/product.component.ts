90c747bf80cff3d511a5494703234b4880359146
Alex Green <alexgreen0606@gmail.com>
2022-12-29 15:54:41 -0600

    finished adding ability to modify products

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -14,61 +14,106 @@ export class ProductComponent {
 
   @Input() product: Product | null = null;
 
+  public todayDate = new Date();
+
+  public name = ''
+  public description = ''
+  public price = 0
+  public available = this.todayDate;
+  public priceVal = 0;
+  public dateVal = this.todayDate;
+  public shipQuantity = 0;
+  public shipCost = 0;
+  public imageURL = ''
+
   public edit = false;
+  public changeName = false;
+  public changeDesc = false;
+  public changePrice = false;
+  public changeAvail = false;
+  public schedMAP = false;
+  public schedPrice = false;
+  public schedSale = false;
+  public addShip = false;
+  public addIm = false;
+  public addCat = false;
 
-  public categories: Categories[] = []
+  constructor(private service: ProductService) { }
 
-  public images: string[] = []
-
-  public scheduledMAPS : Price[] = []
-  public scheduledPrices : Price[] = []
-  public scheduledSales : Price[] = []
-
-  public shipments : Shipment[] = []
-
-  constructor(private service : ProductService){}
-
-  editProduct(){
-    this.edit = true;
-  }
-
-  addCategory(category: Categories){
-    this.categories.push(category);
-  }
-
-  addImage(image: string){
-    this.images.push(image);
-  }
-
-  addScheduledMAP(limit: number, endDate: Date){
-    this.scheduledMAPS.push(new Price(Math.random(),limit,endDate))
-  }
-
-  addScheduledPrice(price: number, endDate: Date){
-    this.scheduledPrices.push(new Price(Math.random(),price,endDate));
-  }
-
-  addScheduledSale(salePrice: number, endDate: Date){
-    this.scheduledSales.push(new Price(Math.random(),salePrice,endDate));
-  }
-
-  addShipment(quantity: number, cost: number){
-    if(this.product){
-      if(this.product.id)
-      this.shipments.push(new Shipment(Math.random(),this.product.id,quantity,cost));
+  updateName() {
+    if (this.product) {
+      this.product.name = this.name;
+      this.changeName = false;
     }
   }
 
-  updateProduct(){
-    if(this.product){
-      
-      this.product.categories = this.product.categories.concat(this.categories);
-      this.product.images = this.product.images.concat(this.images);
-      this.product.schedulesMAPS = this.product.schedulesMAPS.concat(this.scheduledMAPS);
-      this.product.scheduledPrices = this.product.scheduledPrices.concat(this.scheduledPrices);
-      this.product.scheduledSales = this.product.scheduledSales.concat(this.scheduledSales);
-      this.product.shipments = this.product.shipments.concat(this.shipments);
+  updateDescription() {
+    if (this.product) {
+      this.product.description = this.description;
+      this.changeDesc = false;
+    }
+  }
 
+  updatePrice() {
+    if (this.product) {
+      this.product.price = this.price;
+      this.changePrice = false;
+    }
+  }
+
+  updateAvail() {
+    if (this.product) {
+      this.product.availability = this.available;
+      this.changeAvail = false;
+    }
+  }
+
+  addCategory(category: Categories) {
+    // TODO: add the category
+    this.addCat = false;
+  }
+
+  addImage() {
+    if(this.product)
+    this.product.images.push(this.imageURL)
+    this.addIm = false;
+  }
+
+  addScheduledMAP() {
+    if(this.product)
+    this.product.schedulesMAPS.push(new Price(Math.random(), this.priceVal, this.dateVal))
+    this.schedMAP = false;
+    this.priceVal = 0;
+    this.dateVal = this.todayDate;
+  }
+
+  addScheduledPrice() {
+    if(this.product)
+    this.product.scheduledPrices.push(new Price(Math.random(), this.priceVal, this.dateVal));
+    this.schedPrice = false;
+    this.priceVal = 0;
+    this.dateVal = this.todayDate;
+  }
+
+  addScheduledSale() {
+    if(this.product)
+    this.product.scheduledSales.push(new Price(Math.random(), this.priceVal, this.dateVal));
+    this.schedSale = false;
+    this.priceVal = 0;
+    this.dateVal = this.todayDate;
+  }
+
+  addShipment() {
+    if (this.product) {
+      if (this.product.id)
+        this.product.shipments.push(new Shipment(Math.random(), this.product.id, this.shipQuantity, this.shipCost));
+    }
+    this.addShip = false;
+  }
+
+  // save the modified product to the backend
+  saveProduct() {
+    if (this.product) {
       this.service.updateProduct(this.product);
       this.edit = false;
     }
