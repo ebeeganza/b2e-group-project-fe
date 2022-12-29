3c9dd0dcc5b266192773a4e5adc612f55e2bb000
yorkmacbook094 <connor.nicoski@outlook.com>
2022-12-29 12:03:17 -0600

    Fixed multiple form field modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,7 +23,6 @@ import { RegisterComponent } from './components/register/register.component';
 import { OrdersComponent } from './components/orders/orders.component';
 import { CartComponent } from './components/cart/cart.component';
 import { CreateProductComponent } from './components/create-product/create-product.component';
-import {MatFormFieldModule} from '@angular/material/form-field';
 
 
 @NgModule({
