1c5e05a59cb5eaeb6a9828b789251a829ef298a2
ZachRauch <zach.rauch@outlook.com>
2022-12-29 11:02:18 -0600

    Added orders data and component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatBadgeModule } from '@angular/material/badge';
 import { ProductComponent } from './components/product/product.component';
+import { OrdersComponent } from './components/orders/orders.component';
 
 
 
@@ -19,7 +20,8 @@ import { ProductComponent } from './components/product/product.component';
   declarations: [
     AppComponent,
     ToolbarComponent,
-    ProductComponent
+    ProductComponent,
+    OrdersComponent
   ],
   imports: [
     BrowserModule,
