33b4230d4e3667ec9d7ed2ca6ccaeac9a87c73b5
Patrick <patrickjamesallard@gmail.com>
2023-01-03 15:55:07 -0700

    added view privilages and functionality to account view

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -12,5 +12,20 @@ export class ProfileComponent {
 
   @Input() account: User | undefined
 
-  constructor(public accountService: AccountService, public ui: UiService) {}
+  public id: number
+  public fname: string
+  public lname: string
+  public email: string
+  public password: string
+  public role: number
+
+
+  constructor(public accountService: AccountService, public ui: UiService) {
+    this.id = this.accountService.currentUser.value.id
+    this.fname = this.accountService.currentUser.value.fname
+    this.lname = this.accountService.currentUser.value.lname
+    this.email = this.accountService.currentUser.value.email
+    this.password = this.accountService.currentUser.value.password
+    this.role = this.accountService.currentUser.value.role
+  }
 }
