7cb4eca7bdd91796f30504e66599855b614985aa
yorkmacbook094 <connor.nicoski@outlook.com>
2023-01-04 09:27:53 -0600

    Fixed Navigation on Register Component

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { User } from 'src/app/data/user';
 import { AccountService } from 'src/app/services/account.service';
+import { UiService } from 'src/app/services/ui.service';
 
 @Component({
   selector: 'app-register',
@@ -12,7 +13,7 @@ export class RegisterComponent {
   public newUser: User = new User(-1,'','','','',-1)
 
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, public ui: UiService) { }
 
   registerUser() {  
     this.accountService.registerUser(this.newUser)
