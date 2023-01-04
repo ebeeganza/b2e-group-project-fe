83eadab987c4c95644b65dcd902228dd8bf2d2fd
yorkmacbook098 <cyaj96@gmail.com>
2023-01-04 15:40:23 -0600

    crud for cart

--- src/app/services/cart.service.service.ts
+++ src/app/services/cart.service.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, Input } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import { AccountService } from './account.service';
+import { BehaviorSubject, take } from 'rxjs';
+import { Cart } from '../data/cart';
 import { ProductService } from './product.service';
 
 @Injectable({
@@ -10,46 +10,105 @@ import { ProductService } from './product.service';
 export class CartServiceService {
 
  productList = new BehaviorSubject<any>([]);
- @Input() cartItemList: any[]=[]
+ @Input() cartProductList: any[]=[]
+ 
+ public cartSubject: BehaviorSubject<Cart[]> = new BehaviorSubject<Cart[]>([])
 
-  constructor(private http: HttpClient, private productService : ProductService) { }
+  constructor(private http: HttpClient, private productService : ProductService) { 
+    this.getOnCart()
+    this.http = http
+  }
 
   //get the product
   getProducts() {
-    return this.productList;
+    return this.productList.asObservable();
   }
 
+  getOnCart(): void {
+    this.http.get<Cart[]>(`http://localhost:8080/cart`)
+      .pipe(take(1))
+      .subscribe({
+        next: (carts) => this.cartSubject.next(carts),
+        error: (err) => console.log("Error getting categories")
+      })
+  }
+
+  public addNewCart (id: number, UserId:number, Product:[]): void{
+    this.http.post(`http://localhost:8080/cart`,{
+      id: null,
+      UserId,
+      Product,
+    })
+      .pipe(take(1))
+      .subscribe({
+          next: () => {
+            console.log(id, UserId, Product)
+            console.log("Successfully deleted a Product!")
+          },
+          error:() =>{
+            console.log("Oops, something went wrong on the server side")
+        }
+        })
+  }
+
+  updateProduct(cart: Cart) {
+    this.http.put("http://localhost:8080/products", cart)
+      .pipe(take(1))
+      .subscribe({
+        next: () => {
+          this.getOnCart();
+        },
+        error: (error) => {
+          this.getOnCart();
+        }
+      })
+  }
+  
+  deleteOnCart(id:number){
+    this.http.delete(`http://localhost:8080/cart/${id}`)
+    .pipe(take(1))
+    .subscribe({
+      next: () => {
+        console.log("Successfully deleted a Product!")
+      },
+      error: () => {
+        console.log("Oops, something went wrong on the server side")
+      }
+    })
+  }
+
+
   setProduct(product:any){
-    this.cartItemList.push(...product);
+    this.cartProductList.push(...product);
     this.productList.next(product);
   }
 
   //adding product to chart
   addToCart(product: any){
-    this.cartItemList.push(product)
-    this.productList.next(this.cartItemList)
+    this.cartProductList.push(product)
+    this.productList.next(this.cartProductList)
     this.getTotalPrice();
-    console.log(this.cartItemList)
+    console.log(this.cartProductList)
   }
 
   getTotalPrice(): number {
     let grandTotal = 0;
-    this.cartItemList.map((a:any) => {
+    this.cartProductList.map((a:any) => {
       grandTotal += this.productService.getCurrentPrice(a);
       })
       return grandTotal;
     }
 
-  removeCartItem(product: any){
-    const index = this.cartItemList.indexOf(product);
-    this.cartItemList.splice(index,1)
-    this.productList.next(this.cartItemList)
+  removeCartProduct(product: any){
+    const index = this.cartProductList.indexOf(product);
+    this.cartProductList.splice(index,1)
+    this.productList.next(this.cartProductList)
   }
   
-    //clear Cart if want
+  //clear Cart if want
   removeAllCart(){ 
-    this.cartItemList = [];
-    this.productList.next(this.cartItemList);
+    this.cartProductList = [];
+    this.productList.next(this.cartProductList);
   }
 
   checkout(){
